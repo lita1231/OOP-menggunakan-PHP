935412524514118e5ed4c9c34d7e7b9826b865ee
Enjelita <enjelitasitorus8@gmail.com>
2024-04-13 13:42:24 +0700

    Create config.php

---
+++ phpmvc/app/config/config.php
@@ -0,0 +1,9 @@
+<?php
+
+define('BASEURL', 'http://localhost/OOP-menggunakan-PHP/phpmvc/public/');
+
+// DB
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASS', '');
+define('DB_NAME', 'phpmvc');
