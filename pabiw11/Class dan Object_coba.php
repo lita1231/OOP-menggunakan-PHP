a707600df2592a12f7abe4c43f78c4232f7d352d
Enjelita <enjelitasitorus8@gmail.com>
2024-04-13 13:39:48 +0700

    Create Class dan Object_coba.php

---
+++ pabiw11/Class dan Object_coba.php
@@ -0,0 +1,17 @@
+<?php
+// Nama : Enjelita Sitorus
+// NIM  : 13323023
+
+class Coba {
+
+    
+}
+
+
+$a = new Coba();
+$b = new Coba();
+$c = new Coba();
+
+var_dump($a);
+var_dump($b);
+var_dump($c);
